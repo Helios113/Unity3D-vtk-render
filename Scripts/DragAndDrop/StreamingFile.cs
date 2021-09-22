ae5b5d4a0faca8ae09b718d8a978832bdb3991b3
Preslav Aleksandrov <preslav.aleksandrov@protonmail.com>
2021-09-22 07:51:41 +0100

    Removed Legacy files

--- Scripts/DragAndDrop/StreamingFile.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class StreamingFile : MonoBehaviour
-{
-    [SerializeField] private Object streamingAsset;
-    [SerializeField] private string filePath;
-}
